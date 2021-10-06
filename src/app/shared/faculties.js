4c0930ce3791ffcf0e7855bf4ae29b2086da05de
dasha <misery.hatred.anger@gmail.com>
2021-10-06 20:40:17 +0300

    All requirements are met

---
+++ src/app/shared/faculties.js
@@ -0,0 +1,46 @@
+export const faculties = [
+  {
+    name: 'Faculty of Biology and Soil Science',
+    description: ['biology', 'soil', 'life']
+  },
+  {
+    name: 'Faculty of Chemistry and Chemical Technology',
+    description: ['chemistry', 'chemical', 'substance', 'biopharmaceutics']
+  },
+  {
+    name: 'Faculty of Law',
+    description: ['law', 'criminal', 'civil', 'rights']
+  },
+  {
+    name: 'Faculty of Physics and Engineering',
+    description: ['physics', 'engineering', 'IT']
+  },
+  {
+    name: 'Faculty of History and Philosophy',
+    description: ['history', 'philosophy', 'archaeology', 'anthropology']
+  },
+  {
+    name: 'Faculty of Journalism and Communication Sciences',
+    description: ['journalism', 'communication', 'TV', 'social media']
+  },
+  {
+    name: 'Faculty of Letters',
+    description: ['letters', 'french', 'english', 'romanian', 'russian', 'german', 'spanish', 'translation']
+  },
+  {
+    name: 'Faculty of Mathematics and Computer Science',
+    description: ['IT', 'mathematics', 'computer', 'computer science']
+  },
+  {
+    name: 'Faculty of Psychology and Education Sciences, Sociology and Social Work',
+    description: ['psychology', 'education', 'sociology', 'social work', 'pedagogy', 'family']
+  },
+  {
+    name: 'Faculty of International Relations, Political Sciences and Public Administration',
+    description: ['international', 'politics', 'management', 'administrative']
+  },
+  {
+    name: 'Economic Sciences',
+    description: ['economics', 'tourism', 'business', 'accounting', 'marketing']
+  },
+]
